7b7aef1af01c58f62dc4148a1908cd81e0b49253
Quentin Roy-Foster <16qrf1@caslab.queensu.ca>
2020-02-01 12:51:56 -0500

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("./Processed_Data/SchoolStats/school_2015.csv")
-
-data['School'] = data['School'].replace('( NCAA)', '', regex=True)
-
-data['School'] = data['School'].replace('(State)', 'St', regex=True)
-
-print(data)
